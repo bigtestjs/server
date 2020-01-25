9671aabedf594b46bf6bbfe5a1c4bcf16ec24150
Charles Lowell <cowboyd@frontside.io>
2020-01-25 17:10:41 -0600

    Fix the watcher (kinda)
    When processes complete, it still doesn't really work.

--- bin/watch.ts
+++ bin/watch.ts
@@ -1,67 +1,43 @@
 import { watch } from 'fs';
-import { spawn } from 'child_process';
-import { main, fork, join, monitor, send, receive, Operation, Context } from 'effection';
+import { main, fork, monitor, send, receive, Operation, Context } from 'effection';
 import { on } from '@effection/events';
 
-function* start(): Operation {
+import { spawnProcess } from '../src/effection/child_process';
+
+import { interruptable } from './interruptable';
+
+main(interruptable(function* () {
   let [ cmd, ...args ] = process.argv.slice(2);
-
-  let supervisor: Context = yield fork(function*() {
-    yield monitor(function* changes() {
-      let watcher = watch('src', { recursive: true });
-      try {
-        while (true) {
-          yield on(watcher, "change");
-          console.log('change detected, restarting....');
-          send("restart", supervisor);
-        }
-      } finally {
-        watcher.close();
-      }
-    })
-
-    do {
-      let proc = yield fork(launch(cmd, args));
-      yield receive("restart");
-      proc.halt();
-    } while (true);
-  });
-}
-
-function* launch(cmd: string, args: string[]): Operation {
-  let child = spawn(cmd, args, { stdio: 'inherit'});
-
-  yield fork(function*() {
-    let errors = yield fork(function*() {
-      let [ error ] = yield on(child, "error");
-      throw error;
-    });
-
-    try {
-      let [ code ] = yield on(child, 'exit');
-      errors.halt();
-
-      if (code > 0) {
-        throw new Error(`exited with code ${code}`)
-      }
-    } finally {
-      child.kill();
-    }
-  })
-}
-
-main(function* () {
-  let child = yield fork(start);
-
-  yield monitor(function*() {
-    yield on(process, 'SIGINT');
-    console.log('');
-    child.halt();
-  });
-
   try {
-    yield join(child);
+    while (true) {
+      let child = yield fork(launch(cmd, args));
+      yield startWatching(child);
+
+      yield receive("restart", child);
+      child.halt();
+    }
   } catch (err) {
     console.error(err);
   }
-});
+}));
+
+function launch(cmd: string, args: string[]): Operation {
+  return spawnProcess(cmd, args, { stdio: 'inherit' }, function*(child) {
+    return yield on(child, 'exit');
+  });
+}
+
+function startWatching(child: Context): Operation {
+  return monitor(function* changes() {
+    let watcher = watch('src', { recursive: true });
+    try {
+      while (true) {
+        yield on(watcher, "change");
+        console.log('change detected, restarting....');
+        yield send("restart", child);
+      }
+    } finally {
+      watcher.close();
+    }
+  })
+}
