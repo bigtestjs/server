09d54637c558a8b3e24a77e7127b6be972b72223
Charles Lowell <cowboyd@frontside.io>
2020-02-05 14:12:45 -0600

    Checkpoint with different ideas

--- src/schema.ts
+++ src/schema.ts
@@ -5,6 +5,7 @@ type Query {
   echo(text: String!): String
   agents: [Agent!]!
   manifest: [ManifestEntry!]!
+  runs: [TestRun]!
 }
 
 type Agent {
@@ -41,5 +42,52 @@ type ManifestEntry {
   test: Test!
 }
 
-scalar Test
+type Identifier {
+  path: [String!]!
+}
+
+type Test {
+  id: Identifier!
+  parent: Identifier!
+  description: String!
+  steps: [Step!]!
+  assertions: [Assertion!]
+}
+
+type Step {
+  id: Identifier!
+  description: String!
+}
+
+type Assertion {
+  id: Identifier!
+  descritption: String!
+}
+
+type Mutation {
+  runAll: [TestRun]!
+}
+
+type TestRun {
+  id: ID!
+  agent: Agent!
+  manifest: [ManifestEntry]!
+  results: [TestResult!]!
+}
+
+type TestResult {
+  test: Test!
+  steps: [StepResult!]!
+  assertions: [AssertionResult!]
+}
+
+type StepResult {
+  stepId: Identifier!
+  description: String!
+}
+
+type AssertionResult {
+  stepId: Identifier!
+  description: String!
+}
 `);
